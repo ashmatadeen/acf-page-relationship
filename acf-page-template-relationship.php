5379ed62d0433795715da57e256eb9e8f86a26b8
ash matadeen <ash.matadeen@gmail.com>
2024-04-27 16:15:56 +0100

    base plugin php file

---
+++ acf-page-template-relationship.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: ACF Page Relationship Field
+ * Plugin URI: https://profiles.wordpress.org/ashmatadeen/
+ * Description: Adds a page relationship field to ACF with the ability to filter by page template.
+ * Author: Ash Matadeen
+ * Author URI: https://profiles.wordpress.org/ashmatadeen/
+ * Version: 1.0
+ *
+ * @package ACF Page Relationship Field
+ */
+
+require_once plugin_dir_path( __FILE__ ) . '/init.php';
